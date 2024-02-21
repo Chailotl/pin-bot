8aea5a08e95c25960e59f89c89d34fe8ea3deda2
Chai <7232280+Chailotl@users.noreply.github.com>
2024-02-20 22:15:53 -0500

    Removed unneeded code

--- reaction-bot/Program.cs
+++ reaction-bot/Program.cs
@@ -1,12 +1,9 @@
 ﻿using Discord;
-using Discord.Net;
 using Discord.WebSocket;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ReactionBot
@@ -17,7 +14,6 @@ namespace ReactionBot
 		public ulong pinChannel;
 		public string pinEmoji;
 		public int pinAmount;
-		public List<ulong> roles;
 	}
 
 	class Program
@@ -38,11 +34,7 @@ namespace ReactionBot
 				AlwaysDownloadUsers = true
 			});
 			client.Log += Log;
-			client.Ready += Client_Ready;
 			client.ReactionAdded += PinMessage;
-			client.ReactionAdded += AddRole;
-			client.ReactionRemoved += RemoveRole;
-			client.SlashCommandExecuted += SlashCommandHandler;
 
 			await client.LoginAsync(TokenType.Bot, config.token);
 			await client.StartAsync();
@@ -54,45 +46,6 @@ namespace ReactionBot
 		{
 			Console.WriteLine(msg.ToString());
 			return Task.CompletedTask;
-		}
-
-		private async Task Client_Ready()
-		{
-			// Sets up reactions
-			/*IMessageChannel channel = (IMessageChannel)_client.GetChannel(1002206034640769026);
-
-			foreach(ulong id in config.roles)
-			{
-				IMessage message = await channel.GetMessageAsync(id);
-
-				Console.WriteLine(message.Content);
-				Console.WriteLine(message.CleanContent);
-
-				using (StringReader reader = new StringReader(message.Content))
-				{
-					string line;
-					while ((line = reader.ReadLine()) != null)
-					{
-						if (Emoji.TryParse(line.Split(' ')[0], out Emoji emoji))
-						{
-							await message.AddReactionAsync(emoji);
-						}
-					}
-				}
-			}*/
-
-			var pingCommand = new SlashCommandBuilder()
-				.WithName("ping")
-				.WithDescription("Ping the bot");
-
-			try
-			{
-				await client.CreateGlobalApplicationCommandAsync(pingCommand.Build());
-			}
-			catch (HttpException e)
-			{
-				Console.WriteLine(JsonConvert.SerializeObject(e.Errors, Formatting.Indented));
-			}
 		}
 
 		private async Task PinMessage(Cacheable<IUserMessage, ulong> cachedMessage, Cacheable<IMessageChannel, ulong> cachedChannel, SocketReaction reaction)
@@ -145,109 +98,5 @@ namespace ReactionBot
 				await pinChannel.SendMessageAsync(embed: embedBuilder.Build());
 			}
 		}
-
-		private async Task AddRole(Cacheable<IUserMessage, ulong> cachedMessage, Cacheable<IMessageChannel, ulong> cachedChannel, SocketReaction reaction)
-		{
-			ModifyRole(true, cachedMessage, cachedChannel, reaction);
-		}
-
-		private async Task RemoveRole(Cacheable<IUserMessage, ulong> cachedMessage, Cacheable<IMessageChannel, ulong> cachedChannel, SocketReaction reaction)
-		{
-			ModifyRole(false, cachedMessage, cachedChannel, reaction);
-		}
-
-		private async void ModifyRole(bool addRole, Cacheable<IUserMessage, ulong> cachedMessage, Cacheable<IMessageChannel, ulong> cachedChannel, SocketReaction reaction)
-		{
-			// Ignore non-guilds
-			IMessageChannel channel = await cachedChannel.GetOrDownloadAsync();
-			if (!(channel is IGuildChannel)) { return; }
-
-			// Ignore bots
-			IGuildUser user = await ((IGuildChannel)channel).Guild.GetUserAsync(reaction.UserId);
-			if (user.IsBot) { return; }
-
-			// Check if message has reaction roles
-			IUserMessage message = await cachedMessage.GetOrDownloadAsync();
-
-			if (config.roles.Contains(message.Id))
-			{
-				// Get data
-				IEmote emote = reaction.Emote;
-
-				// Check which reaction role should be given
-				using (StringReader reader = new StringReader(message.Content))
-				{
-					string line;
-					while ((line = reader.ReadLine()) != null)
-					{
-						// Add corresponding role
-						if (line.Contains(emote.Name))
-						{
-							Regex rg = new Regex(@"<@&(\d+)>");
-							Match match = rg.Match(line);
-							if (match.Success)
-							{
-								if (addRole)
-								{
-									await user.AddRoleAsync(ulong.Parse(match.Groups[1].Value));
-								}
-								else
-								{
-									await user.RemoveRoleAsync(ulong.Parse(match.Groups[1].Value));
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-
-		private async Task SlashCommandHandler(SocketSlashCommand command)
-		{
-			switch (command.Data.Name)
-			{
-				case "ping":
-					await command.RespondAsync("Pong!", ephemeral: true);
-					break;
-			}
-		}
-
-		/*private async Task HandleColorCommand(SocketSlashCommand command)
-		{
-			SocketGuildUser user = (SocketGuildUser)command.User;
-			SocketGuild guild = user.Guild;
-			string roleName = $"USER-{user.Id}";
-
-			// Get color
-			string hex = command.Data.Options.First().Value.ToString();
-			hex = hex.StartsWith('#') ? hex.Substring(1) : hex;
-			Color color = new Color((uint)int.Parse(hex, System.Globalization.NumberStyles.HexNumber));
-
-			// Find role
-			SocketRole role = guild.Roles.Where(x => x.Name == roleName).FirstOrDefault();
-
-			if (role != null)
-			{
-				// Change role color and assign it if they're missing it
-				await role.ModifyAsync(x => x.Color = color);
-
-				if (!user.Roles.Contains(role))
-				{
-					await user.AddRoleAsync(role);
-				}
-			}
-			else
-			{
-				// Create and assign role
-				await user.AddRoleAsync(await guild.CreateRoleAsync(roleName, color: color, isMentionable: false));
-			}
-
-			// Send response
-			EmbedBuilder embedBuilder = new EmbedBuilder()
-				.WithDescription($"Your role color was changed to **{'#' + hex}**")
-				.WithColor(color);
-
-			await command.RespondAsync(embed: embedBuilder.Build(), ephemeral: true);
-		}*/
 	}
 }
